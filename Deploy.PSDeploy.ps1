680ae924edda5db9ae5fd543922425d4d8e0f091
Chris Stone <chris@stoneyforest.net>
2020-06-11 13:46:34 -0400

    ## 2020-06-11 #7

--- Deploy.PSDeploy.ps1
+++ Deploy.PSDeploy.ps1
@@ -14,13 +14,9 @@
  # Set-BuildEnvironment from BuildHelpers module has populated ENV:BHProjectName
 
 # Publish to gallery with a few restrictions
-if(
-    $env:BHPSModulePath -and
+If ($env:BHPSModulePath -and
     $env:BHBuildSystem -ne 'Unknown' -and
-    $env:BHBranchName -eq "master" -and
-    $env:BHCommitMessage -match '!deploy'
-)
-{
+    @('master','devel') -contains $env:BHBranchName ) {
     Deploy Module {
         By PSGalleryModule {
             FromSource $ENV:BHPSModulePath
@@ -30,29 +26,10 @@ if(
             }
         }
     }
-}
-else
-{
+} Else {
     "Skipping deployment: To deploy, ensure that...`n" +
     "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
     "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
     "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
         Write-Host
-}
-
-# Publish to AppVeyor if we're in AppVeyor
-if(
-    $env:BHPSModulePath -and
-    $env:BHBuildSystem -eq 'AppVeyor'
-   )
-{
-    Deploy DeveloperBuild {
-        By AppVeyorModule {
-            FromSource $ENV:BHPSModulePath
-            To AppVeyor
-            WithOptions @{
-                Version = $env:APPVEYOR_BUILD_VERSION
-            }
-        }
-    }
 }
